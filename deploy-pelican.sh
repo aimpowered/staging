910ef8801fd7858715983e5da6cbca2d2a63aa62
Shaomei <shaomei@gmail.com>
2021-09-08 12:47:32 -0700

    Try out using a soft link in the repo

--- deploy-pelican.sh
+++ deploy-pelican.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # This script is forked from the original version in the Notes app.
 
-cd "$(dirname "$0")/Pelican
-pelican content -o .. -s pelicanconf.py
-cd ..
-cp about.html index.html
-git checkout main
-git add --all
-git commit -m “updating pelican outputs"”
-git push origin main
+set -ex
 
+function generate() {
+	pelican Pelican/content -s Pelican/pelicanconf.py -o docs -d
+	rm docs/index.html
+	ln -s pages/about-us.html docs/index.html
+}
+
+function deploy() {
+	git checkout main
+	git add --all
+	git commit -m "updating pelican outputs"
+	git push origin main
+}
+
+function main() {
+	generate
+	# deploy
+}
+
+main "$@"
